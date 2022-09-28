d858912046cefad584e81d58db03d9afd202743d
Rupali Bhimrao Sonawane <83916233+RupaliS218@users.noreply.github.com>
2022-09-28 23:16:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Fetchdata from "./Fetchdata";
+
+function App() {
+  return (
+    <div className="App">
+      <Fetchdata />
+    </div>
+  );
+}
+
+export default App;
